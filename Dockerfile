77f901f2231ff38f9b330ed5c7242792af56492e
zackees <z@zackees.com>
2025-05-05 13:48:48 -0700

    Add wormhole-tx installation and update docker compose commands

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,16 @@
 FROM python:3.13-slim
 
 RUN apt-get update
-RUN apt-get install djvulibre-bin poppler-utils
+RUN apt-get install -y djvulibre-bin
+RUN apt-get install -y poppler-utils
+
+
+RUN pip install wormhole-tx
 
 
 WORKDIR /app
 
 COPY app.py .
+COPY test_data ./test_data
 
 CMD ["python", "app.py"]
--- run.py
+++ run.py
@@ -1,4 +1,14 @@
+#!/usr/bin/env -S uv run --script
+
+# /// script
+# dependencies = [
+#
+# ]
+# ///
+
 import os
 
 os.system('docker compose down --rmi=all')
-os.system('docker compose up --build')
+os.system('docker compose up --build --no-start')
+
+os.system('docker compose run --rm --service-ports --entrypoint /bin/bash app')

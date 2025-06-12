31288cbd437959d9dd8db5a301783b5982d3ccc5
zackees <z@zackees.com>
2025-06-12 16:00:04 -0700

    Update version to 1.0.11 and refactor docker test script

--- tests/integration/docker_run.py
+++ tests/integration/docker_run.py
@@ -6,12 +6,10 @@ from pathlib import Path
 
 import os
 import unittest
+import subprocess
 
-
-# os.system('docker compose down --rmi=all')
-# os.system('docker compose up --build --no-start')
-
-# os.system('docker compose run --rm --service-ports --entrypoint /bin/bash app')
+from tempfile import TemporaryDirectory
+from pdf_ingest.cli import main as cli_main
 
 
 class EpubTester(unittest.TestCase):
@@ -25,8 +23,29 @@ class EpubTester(unittest.TestCase):
 
     def test_sanity(self) -> None:
         # test that pyproject.toml is present at the root
-        os.system("docker compose down --rmi=all")
-        os.system("docker compose up --build")
+        # cli_main()
+
+        test_data = Path("test_data")
+        self.assertTrue(test_data.exists(), f"Expected {test_data} to exist.")
+
+        with TemporaryDirectory() as temp_dir:
+            out_dir: Path = Path(temp_dir)
+            print(f"Temporary directory created at: {out_dir}")
+
+            arg_list: list[str] = [
+                str(test_data),  # input_dir
+                "--output_dir",
+                str(out_dir),
+                "--depth",
+                "0"
+            ]
+
+            arg_str = subprocess.list2cmdline(arg_list)
+            print("running command with args:", arg_str)
+
+            rtn = cli_main(arg_list)
+            self.assertEqual(rtn, 0, f"Expected return code 0, got {rtn}")
+
 
         
 
